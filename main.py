1d890b2146f35186caf5ac4667118cae648991d5
Gustavo Sene <gustavodev6331@gmail.com>
2026-07-25 13:12:39 -0400

    fix variable name

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import random
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Boolean
-import random
 
 
 # URL FOR POSTMAN: https://documenter.getpostman.com/view/55360170/2sBXwntXXm
@@ -33,6 +35,7 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
     def to_dict(self):
+        """Convert the Cafe model into a serializable dictionary."""
         return {
         "id": self.id,
         "name": self.name,
@@ -55,37 +58,40 @@ def home():
     return render_template("index.html")
 
 @app.route("/random")
-def random_cof():
-    if request.method == "GET":
-        result = db.session.execute(db.select(Cafe))
-        all_caffe = result.scalars().all()
-        random_caffe = random.choice(all_caffe)
-        response = jsonify(cafe=random_caffe.to_dict())
+def random_cafe():
+    """Return a random cafe from the database."""
+    result = db.session.execute(db.select(Cafe))
+    all_cafe = result.scalars().all()
+    random_cafe = random.choice(all_cafe)
+    response = jsonify(cafe=random_cafe.to_dict())
 
-        return response
+    return response
 
 @app.route("/all", methods=["GET", "POST"])
-def all_cof():
+def all_cafes():
+    """Get a list of all cafes."""
     results = db.session.execute(db.select(Cafe))
-    all_caffe = results.scalars().all()
+    all_cafe = results.scalars().all()
 
-    return jsonify(cafes=[cafe.to_dict() for cafe in all_caffe])
+    return jsonify(cafes=[cafe.to_dict() for cafe in all_cafe])
 
 @app.route("/search", methods=["GET", "POST"])
-def search_cof():
+def search_cafe():
+    """search the cafe by spefici location."""
     location = request.args.get("loc")
 
     result = db.session.execute(db.select(Cafe).where(Cafe.location == location))
-    all_caffe = result.scalars().all()
-    if all_caffe:
-        return jsonify(cafes=[cafe.to_dict() for cafe in all_caffe])
+    all_cafe = result.scalars().all()
+    if all_cafe:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafe])
     else:
         return "Sorry, we don't have a cafe at that location."
 
 
 @app.route("/add", methods=["POST"])
-def post_new_caffe():
-    new_caffe = Cafe (
+def post_new_cafe():
+    """add a new cafe to the database."""
+    new_cafe = Cafe (
         name=request.args.get("name"),
         map_url=request.args.get("map_url"),
         img_url= request.args.get("img_url"),
@@ -97,7 +103,7 @@ def post_new_caffe():
         seats = request.args.get("seats"),
         coffee_price = request.args.get("coffee_price"),
     )
-    db.session.add(new_caffe)
+    db.session.add(new_cafe)
     db.session.commit()
     return jsonify(response={'success': "The new cafe has been added."})
 
@@ -113,15 +119,15 @@ def update_price(coffe_id):
         return jsonify(response={'success': "The coffe has been updated."}), 200
 
 @app.route("/delete/<int:coffe_id>", methods=["DELETE"] )
-def delete_caffe(coffe_id):
+def delete_cafe(coffe_id):
     api_key = request.args.get("api_key")
 
     if api_key != "TopSecretAPIKey":
-        return jsonify(response={'not found': "You can not delete data without API key."}), 404
+        return jsonify(response={'not found': "You can not delete data without API key."}), 403
     coffe = db.session.get(Cafe, coffe_id)
     print(coffe_id)
     if coffe is None:
-        return jsonify(response={'not found': "that coffe id does not exist in our database."}), 404
+        return jsonify(response={'not found': "that coffe id does not exist in our database."}), 403
 
     else:
         db.session.delete(coffe)
